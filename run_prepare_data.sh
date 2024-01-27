fd7d1a940433530671adcdb57ecfa525fbfe098b
wujianP <1051233700@qq.com>
2024-01-27 16:32:03 +0800

    test

---
+++ run_prepare_data.sh
@@ -0,0 +1,5 @@
+# 准备数据（解压到share memory中）
+python /DDN_ROOT/ytcheng/tools/k600_init.py
+python /DDN_ROOT/ytcheng/tools/ucf_init.py
+python /DDN_ROOT/ytcheng/tools/hmdb_init.py
+python /DDN_ROOT/ytcheng/tools/k400_init.py
